ed0e9cce33ac1dfbe3e753aeb05f324acbf527f2
sulakshana <sulakshana.sa@gmail.com>
2019-11-11 12:27:21 +0530

    fc

---
+++ src/main/java/TY_maven/Demo_Maven/App.java
@@ -0,0 +1,13 @@
+package TY_maven.Demo_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/TY_maven/Demo_Maven/TestMaven.java
@@ -0,0 +1,12 @@
+package TY_maven.Demo_Maven;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestMaven {
+	@Test
+	public void testmvn() {
+		Reporter.log("maven installed",true);
+	}
+
+}
